4c2b119fe2da17b6b6113c9fcc7102c6aeb87900
Mariano Lischetti <lischettim@gmail.com>
2016-06-09 20:58:56 -0300

    Parentheses balance

---
+++ week1/mlischetti/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,43 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = ???
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+
+    def isParenthesesBalanced(balanceOfParentheses: Int, chars: List[Char]): Boolean = {
+      if (balanceOfParentheses < 0)
+        return false
+
+      chars match {
+        case Nil => balanceOfParentheses == 0
+        case x :: tail => {
+          val parenthesesFactor = if (x == '(') 1 else if (x == ')') -1 else 0
+          isParenthesesBalanced(balanceOfParentheses + parenthesesFactor, chars.tail)
+        }
+      }
+    }
+
+    isParenthesesBalanced(0, chars)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
